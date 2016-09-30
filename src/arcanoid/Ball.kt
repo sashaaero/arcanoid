caa60a08cb3074a965f93022221308d82c74fd3b
sashaaero <sashaaero@yandex.ru>
2016-09-30 08:04:32 +0300

    Smth

--- src/arcanoid/Ball.kt
+++ src/arcanoid/Ball.kt
@@ -1,8 +1,31 @@
 package arcanoid
 
 class Ball(private val game: Game, var x: Int = 50, var y: Int = 50, val d: Int = 50){
+    /*
+        Class provides an activity of ball
+     */
+    enum class directions {rightup, leftup, leftdown, rightdown}
+    var direction = directions.rightup
+    var speed = 5
+
     fun move(){
-        x++;
-        y++;
+        when (direction){
+            directions.rightup -> {
+                x += speed
+                y -= speed
+            }
+            directions.leftup -> {
+                x -= speed
+                y -= speed
+            }
+            directions.leftdown -> {
+                x -= speed
+                y += speed
+            }
+            directions.rightdown -> {
+                x += speed
+                y += speed
+            }
+        }
     }
 }
